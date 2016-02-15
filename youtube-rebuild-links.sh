d48256af833cfd99841a7674127aa92ab5a65cfd
PotcFdk <PotcFdk@gmx.net>
2016-02-15 20:13:04 +0100

    Save all meta-information in /META * Move titles to /META/*.title * Save descriptions to /META/*.description
    Addresses #4

--- youtube-rebuild-links.sh
+++ youtube-rebuild-links.sh
@@ -7,16 +7,16 @@ fi
 
 mkdir -p SYNC/$1/LINK
 
-if [ ! -d SYNC/$1/TITLE ]; then
-	>&1 echo "Error: /TITLE directory doesn't exist for $1"
+if [ ! -d SYNC/$1/META ]; then
+	>&1 echo "Error: /META directory doesn't exist for $1"
 	exit 1
 fi
 
 rm SYNC/$1/LINK/*.mkv
 
-for titlepath in SYNC/$1/TITLE/*; do
+for titlepath in SYNC/$1/META/*.title; do
 	read -r TITLE < $titlepath
-	if [[ "$titlepath" =~ /([^/]+)$ ]]; then
+	if [[ "$titlepath" =~ /([^/]+)\.title$ ]]; then
 		ID=${BASH_REMATCH[1]};
 		echo "Creating link: $ID -> $TITLE.$ID.mkv"
 		ln -s "../ID/$ID.mkv" "SYNC/$1/LINK/$TITLE.$ID.mkv"
--- youtube-update-metadata.sh
+++ youtube-update-metadata.sh
@@ -7,19 +7,23 @@ fi
 
 FILES=SYNC/$1/ID/*.mkv
 
-mkdir -p SYNC/$1/TITLE
+mkdir -p SYNC/$1/META
 
 for filen in $FILES; do
 	if [ -f "$filen" ]; then
 		if [[ "$filen" =~ /([^/]+)\.mkv$ ]]; then
 			ID=${BASH_REMATCH[1]};
-			echo "Checking video title for $ID...";
+			echo -n "Updating metadata: $ID...";
 			TITLE=$(./youtube-dl --get-filename -o '%(title)s' "$ID");
 			if [ -z "$TITLE" ]; then
-				echo "Error. Video might be down. We'll keep the old link."
+				echo " [Error] Video might be down. We'll keep the old link."
 			else
-				echo "New video title: $TITLE";
-				echo -n $TITLE > SYNC/$1/TITLE/$ID
+				echo " [OK]"
+				echo -n "$TITLE" > SYNC/$1/META/$ID.title
+				DESCRIPTION=$(./youtube-dl --get-description "$ID");
+				if [ -n "$DESCRIPTION" ]; then
+					echo "$DESCRIPTION" > SYNC/$1/META/$ID.description
+				fi
 			fi
 		fi
 	fi
